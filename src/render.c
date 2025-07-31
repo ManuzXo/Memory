ff3894fe4f97947839a06b7b34df0587de2930ba
emanuele_rispoli_2712 <emanuelerispoli2712@gmail.com>
2025-07-31 10:07:54 +0200

    CMakeList aggiornato con header file, e aggiunti file render

---
+++ src/render.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "raylib/raylib.h"
+#include "render.h"
+void RenderInit() {
+	InitWindow(800, 800, "Basic Window");
+	SetWindowState(FLAG_WINDOW_RESIZABLE);
+	SetTargetFPS(60);
+
+	RenderLoop();
+	RenderClear();
+}
+void RenderLoop() {
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		RenderInfo();
+		EndDrawing();
+	}
+}
+void RenderInfo() {
+	char fpsTxt[100];
+	int posX = (GetScreenWidth() / 2) - (sizeof(fpsTxt) / 2);
+	int posY = GetScreenHeight() / 2;
+	sprintf(fpsTxt, "FPS: %d", GetFPS());
+	DrawText(fpsTxt, posX, posY, 20, BLACK);
+}
+
+void RenderClear() {
+	CloseWindow();
+}
---
+++ src/render.h
@@ -0,0 +1,7 @@
+#ifndef _RENDER_
+#define _RENDER_
+extern void RenderInit();
+extern void RenderLoop();
+extern void RenderInfo();
+extern void RenderClear();
+#endif // !_RENDER_
